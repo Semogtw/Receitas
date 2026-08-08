1d2aafd96c7ba37c9b5f449c5a50881852a3bbe4
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-07 23:10:29 -0300

    feat: compose browser media runtime

---
+++ src/features/media/create-browser-media-runtime.ts
@@ -0,0 +1,29 @@
+import type { PowerSyncDatabase } from '@powersync/web'
+import { getSupabaseClient } from '../../lib/supabase/client'
+import type { RecipeRepositoryScope } from '../recipes/data/recipe-repository'
+import { BrowserMediaBlobCache } from './browser/media-blob-cache'
+import { prepareImageForUpload } from './browser/prepare-image'
+import { MediaUploadQueueStore } from './data/media-upload-queue'
+import { PhotoMetadataPublisher } from './data/photo-metadata-publisher'
+import { SupabaseMediaDownload } from './data/supabase-media-download'
+import { SupabaseMediaStorage } from './data/supabase-media-storage'
+import { MEDIA_STORAGE_BUCKET } from './media-config'
+import { MediaRuntime } from './media-runtime'
+
+export function createBrowserMediaRuntime(
+  database: PowerSyncDatabase,
+  scope: RecipeRepositoryScope,
+): MediaRuntime {
+  const client = getSupabaseClient()
+  const cache = new BrowserMediaBlobCache()
+  const queue = new MediaUploadQueueStore(database)
+
+  return new MediaRuntime({
+    cache,
+    queue,
+    storage: new SupabaseMediaStorage(client, MEDIA_STORAGE_BUCKET),
+    metadata: new PhotoMetadataPublisher(database, scope),
+    remote: new SupabaseMediaDownload(client, MEDIA_STORAGE_BUCKET),
+    prepare: prepareImageForUpload,
+  })
+}
