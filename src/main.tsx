c983afb945595f527ef69eafc587f30790dafc73
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-08 23:32:06 -0300

    style: load shopping workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './styles/auth.css'
 import './styles/recipes.css'
 import './styles/recipe-controls.css'
 import './styles/planner.css'
+import './styles/shopping.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Application root element is missing')
