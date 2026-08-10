b403720a5cf8a1001ab49034fc7a7aebd460f1ab
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-10 04:14:32 -0300

    style: include trash recovery styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './app/App'
 import './styles/tokens.css'
 import './styles/themes.css'
 import './styles/base.css'
@@ -16,7 +15,11 @@ import './styles/imports.css'
 import './styles/backup.css'
 import './styles/diagnostics.css'
 import './styles/account-admin.css'
+import './styles/trash.css'
+import { App } from './app/App'
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Application root element is missing')
-createRoot(rootElement).render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
